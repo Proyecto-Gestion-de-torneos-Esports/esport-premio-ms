13432b78fe62b9a0583a03c08c53d406a4c56bed
Amaro <godoyamaro59@gmail.com>
2026-06-07 17:01:17 -0400

    mejora en la validacion de los feing client

--- src/main/java/esports/inventarioPremios/client/TorneoClient.java
+++ src/main/java/esports/inventarioPremios/client/TorneoClient.java
@@ -4,7 +4,7 @@ import esports.inventarioPremios.dto.TorneoResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+//@FeignClient(name = “torneos”, url = "http:localhost:8023/api/torneos")
 @FeignClient(name = "torneos", url = "http://localhost:8003/api/torneos")
 public interface TorneoClient {
     @GetMapping("/{id}")
--- src/main/java/esports/inventarioPremios/exception/GlobalExceptionHandler.java
+++ src/main/java/esports/inventarioPremios/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package esports.inventarioPremios.exception;
 
+import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -8,21 +9,47 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> manejarValidaciones(MethodArgumentNotValidException ex) {
-        Map<String, String> errores = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new LinkedHashMap<>();
 
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String nombreCampo = ((FieldError) error).getField();
-            String mensaje = error.getDefaultMessage();
-            errores.put(nombreCampo, mensaje);
-        });
+        ex.getBindingResult().getFieldErrors().forEach((FieldError error) ->
+                errores.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(errores);
+    }
 
-        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> error = new LinkedHashMap<>();
+        error.put("error", ex.getMessage());
+
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(FeignException.NotFound.class)
+    public ResponseEntity<Map<String, String>> handleFeignNotFoundException(FeignException.NotFound ex) {
+        Map<String, String> error = new LinkedHashMap<>();
+        String urlComprobar = ex.request().url();
+
+
+        if (urlComprobar.contains("/api/torneos")) {
+            error.put("error", "Torneo no encontrado");
+            error.put("mensaje", "El torneo no existe o fue eliminado.");
+
+        } else {
+            error.put("error", "Recurso no encontrado");
+            error.put("mensaje", "El recurso solicitado en el microservicio externo no existe.");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
+
+
+
